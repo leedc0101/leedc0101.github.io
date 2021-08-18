b21c3a8445dd3f61b4d304d5df84b8976b7edffb
leedc0101 <leedc0101@naver.com>
2021-08-19 02:06:57 +0900

    Add Header & Footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <Header/>
-    </div>
+      <div>
+          <Header/>
+          <Footer/>
+      </div>
   );
 }
 
