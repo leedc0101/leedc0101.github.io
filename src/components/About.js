79c416de0d83f56bd6da0deb33248d8c0263f511
leedc0101 <leedc0101@naver.com>
2021-08-20 00:54:04 +0900

    Edit about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,71 @@
+import styled from "styled-components";
+import selfie from "../assets/img.jpeg"
+import {introText} from "../texts";
+import {useMediaQuery} from "react-responsive";
+
+const AboutWrap = styled.div`
+  display:grid;
+  grid-template-areas: 
+          "intro image"
+          "links image";
+  margin: 30px;
+  grid-gap :20px;
+  @media only screen and (max-width: 500px){
+    grid-template-areas: 
+            "image"
+            "intro"
+            "links";
+  }
+`
+
+const ImageWrap = styled.div`
+  grid-area: image;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
+const IntroWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  grid-area: intro;
+  line-height: 1.5;
+`
+
+const LinksWrap = styled.div`
+  display: flex;
+  grid-area: links;
+  a {
+    text-decoration: none; 
+  }
+`
+
+const IntroText = styled.div`
+  white-space: pre-line;
+  font-size: 1vw;
+`
+
 function About() {
+    const isBigScreen = useMediaQuery({minDeviceWidth: 500})
+
     return(
-        <div>
-            about
-        </div>
+        <AboutWrap>
+            <IntroWrap>
+                <IntroText>
+                    {introText}
+                </IntroText>
+            </IntroWrap>
+            <ImageWrap>
+                {isBigScreen ? (
+                    <img style={{height:"20vw", width:"20vw"}} src={selfie}/>
+                ) : (
+                    <img style={{height:"30vw", width:"30vw"}} src={selfie}/>
+                )}
+            </ImageWrap>
+            <LinksWrap>
+                <a href="https://velog.io/@leedc0101">velog</a>
+            </LinksWrap>
+        </AboutWrap>
     )
 }
 
