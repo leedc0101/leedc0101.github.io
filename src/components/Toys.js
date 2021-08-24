464bc5dca82ff61f8226dae6074e31241f282be5
leedc0101 <leedc0101@naver.com>
2021-08-25 04:23:18 +0900

    Add new toy project

--- src/components/Toys.js
+++ src/components/Toys.js
@@ -1,6 +1,9 @@
 function Toys() {
     return(
-        <div>not finished</div>
+        <div>
+            <h3>Uniswap Web3-React Practice</h3>
+            <a href="https://leedc0101.github.io/web3react-practice" target="_blank">Link</a>
+        </div>
     )
 }
 
